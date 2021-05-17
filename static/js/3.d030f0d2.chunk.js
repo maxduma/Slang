aad397fd758e2a12b629f19ca24acff49811532d
Max <maxduma16@gmail.com>
2021-05-17 17:18:48 +0300

    Updates

---
+++ static/js/3.d030f0d2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{306:function(p,s,r){p.exports={wrapper:"Music_wrapper__3BsDc"}},309:function(p,s,r){"use strict";r.r(s);r(3);var a=r(306),c=r.n(a),e=r(2);s.default=function(p){return Object(e.jsx)("div",{className:c.a.wrapper,children:"Music"})}}}]);
+//# sourceMappingURL=3.d030f0d2.chunk.js.map
---
+++ static/js/4.92e408f8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[4],{305:function(p,e,r){p.exports={wrapper:"News_wrapper__3GvGW"}},308:function(p,e,r){"use strict";r.r(e);r(3);var s=r(305),a=r.n(s),n=r(2);e.default=function(p){return Object(n.jsx)("div",{className:a.a.wrapper,children:"News"})}}}]);
+//# sourceMappingURL=4.92e408f8.chunk.js.map
